f6b1f8b824f65fb85eae9ec6628a26b747e755dc
Breygas Andara <breiighost@gmail.com>
2022-05-05 11:51:23 +0700

    Initial commit

---
+++ src/main/java/com/andarabrey/arcilla/ArcillaApplication.java
@@ -0,0 +1,13 @@
+package com.andarabrey.arcilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andarabrey/arcilla/ArcillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andarabrey.arcilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

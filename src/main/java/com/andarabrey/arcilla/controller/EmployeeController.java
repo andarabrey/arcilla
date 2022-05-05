7b7d1cec30cbcbe1d95c371039233aedd437936c
Breygas Andara <breiighost@gmail.com>
2022-05-05 13:30:59 +0700

    Add Add Employee method and template view

--- src/main/java/com/andarabrey/arcilla/controller/EmployeeController.java
+++ src/main/java/com/andarabrey/arcilla/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +24,22 @@ public class EmployeeController {
 
         model.addAttribute("employees", employees);
 
-        return "employees-list";
+        return "employees/employees-list";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        Employee employee = new Employee();
+
+        model.addAttribute(employee);
+
+        return "employees/add-employees-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
     }
 }
